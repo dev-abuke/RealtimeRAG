5b767c6b587b094997ffed5a524a6dc96b8cf116
dev-abuke <abubekershamil123@gmail.com>
2024-11-17 13:37:48 +0300

    feat: add byteway output sink to vector db Qdrant cloud

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -20,3 +20,15 @@ def build_input(is_input_mocked: bool = True,):
     if is_input_mocked:
         return TestingSource(mocked.financial_news)
     
+def build_output(model: EmbeddingModelSingleton, in_memory: bool = False):
+    if in_memory:
+        return QdrantVectorOutput(
+            vector_size=model.max_input_length,
+            client=QdrantClient(":memory:"),
+        )
+    else:
+        return QdrantVectorOutput(
+            vector_size=model.max_input_length,
+        )
+    
+    
