2cece2560a8cffabdf5635bf680ff347ec40176c
dev-abuke <abubekershamil123@gmail.com>
2024-11-17 13:34:29 +0300

    fix: import path error fix

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -7,7 +7,7 @@ from bytewax.testing import TestingSource
 
 from qdrant_client import QdrantClient
 
-from ingestion_pipeline import mocked
+from pipeline import mocked
 from pipeline.embeddings import EmbeddingModelSingleton
 from pipeline.models import NewsArticle, Document
 from pipeline.qdrant import QdrantVectorOutput
@@ -19,3 +19,4 @@ model = EmbeddingModelSingleton(cache_dir=None)
 def build_input(is_input_mocked: bool = True,):
     if is_input_mocked:
         return TestingSource(mocked.financial_news)
+    
