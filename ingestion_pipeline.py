080a5a1e5b52dfb74d5f3cd81b51672b002b4fe1
dev-abuke <abubekershamil123@gmail.com>
2024-11-17 13:26:53 +0300

    refactore: rename pipeline for naming issues

---
+++ ingestion_pipeline.py
@@ -0,0 +1,18 @@
+import datetime
+from pathlib import Path
+from typing import List, Optional
+
+from bytewax.dataflow import Dataflow
+from bytewax.testing import TestingSource
+
+from qdrant_client import QdrantClient
+
+from ingestion_pipeline import mocked
+from pipeline.embeddings import EmbeddingModelSingleton
+from pipeline.models import NewsArticle, Document
+from pipeline.qdrant import QdrantVectorOutput
+
+from bytewax import operators as op
+
+model = EmbeddingModelSingleton(cache_dir=None)
+
