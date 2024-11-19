24f161673949d461a8e6b73c6194fa5b20b3a98b
Abubeker Shamil <abubekershamil123@gmail.com>
2024-11-19 10:29:32 +0000

    fix: use bytewaxerror recovery from package not found

--- bytewax_batch.sh
+++ bytewax_batch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# Runs batch stream locally so make sure to install requirements.txt
 
 # Check if the Bytewax module is installed
-if ! python -c "import bytewax" &> /dev/null; then
+if ! python3.11 -c "import bytewax" &> /dev/null; then
   echo "Error: Bytewax is not installed. Please install it using 'pip install bytewax'."
+  python3.11 -m pip install -r requirements.txt
   exit 1
 fi
 
 # Run the Bytewax stream
 echo "Running Bytewax Batch For 1 day.."
-RUST_BACKTRACE=full python -m bytewax.run ingestion_pipeline:build_batch_dataflow
+RUST_BACKTRACE=full python3.11 -m bytewax.run ingestion_pipeline:build_batch_dataflow
 
 if [ $? -eq 0 ]; then
   echo "Bytewax stream is running successfully."
