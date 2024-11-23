6960efe57d595998131b8d97b31cfe8ca53d148c
Abubeker Shamil <abubekershamil123@gmail.com>
2024-11-23 11:21:40 +0000

    refactore: use enviroment variable for secret keys

--- pipeline/constants.py
+++ pipeline/constants.py
@@ -1,19 +1,21 @@
-EMBEDDING_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
-EMBEDDING_MODEL_MAX_INPUT_LENGTH = 384
-EMBEDDING_MODEL_DEVICE = "cpu"
+import os
 
-CROSS_ENCODER_MODEL_ID: str = "cross-encoder/ms-marco-MiniLM-L-6-v2"
+EMBEDDING_MODEL_ID = os.getenv("EMBEDDING_MODEL_ID", "sentence-transformers/all-MiniLM-L6-v2")
+EMBEDDING_MODEL_MAX_INPUT_LENGTH = os.getenv("EMBEDDING_MODEL_MAX_INPUT_LENGTH", 384)
+EMBEDDING_MODEL_DEVICE = os.getenv("EMBEDDING_MODEL_DEVICE", "cpu")
+
+CROSS_ENCODER_MODEL_ID: str = os.getenv("CROSS_ENCODER_MODEL_ID", "cross-encoder/ms-marco-MiniLM-L-6-v2")
 
 # Qdrant Cloud
-QDRANT_URL: str = "https://73bdd42b-86a7-49fc-bcf4-e6bf85cfca17.us-east4-0.gcp.cloud.qdrant.io:6333"
-QDRANT_API_KEY: str = 'pG-09pBVQTDpHkLVL3b3m_A_nEZLGYg88ew8_wFb5BtkasvGpyHOlQ'
-VECTOR_DB_OUTPUT_COLLECTION_NAME = "alpaca_financial_news"
+QDRANT_URL: str = os.environ["QDRANT_URL"]
+QDRANT_API_KEY: str = os.environ["QDRANT_API_KEY"]
+VECTOR_DB_OUTPUT_COLLECTION_NAME = os.getenv("VECTOR_DB_OUTPUT_COLLECTION_NAME", "alpaca_financial_news")  
 
 # OpenAI
-OPENAI_API_KEY: str = "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"
-LLM_MODEL_NAME: str = "gpt-4o-mini"
-LLM_BASE_URL: str = "https://openrouter.ai/api/v1"
+OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
+LLM_MODEL_NAME: str = os.getenv("LLM_MODEL_NAME", "gpt-3.5-turbo")
+LLM_BASE_URL: str = os.getenv("LLM_BASE_URL", "https://api.openai.com/v1")
 
 # Alpaca News
-ALPACA_NEWS_API_KEY: str = "PKM19APHZSD7EDUI20D6"
-APACA_NEWS_SECRET: str = "GifphcRRfVCyc4VTfaTBg9z4MZT5nP3rdZVgkq0x"
+ALPACA_NEWS_API_KEY: str = os.environ["ALPACA_NEWS_API_KEY"]
+APACA_NEWS_SECRET: str = os.environ["ALPACA_NEWS_SECRET"]
